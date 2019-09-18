b82a3c94dd079c9f655659b19b0b161cefd31f52
Quinn Casey <qcasey1@gmail.com>
2019-09-18 15:13:03 -0700

    generalizing config script

--- MDroid-GPS.py
+++ MDroid-GPS.py
@@ -1,6 +1,7 @@
 #! /usr/bin/python
  
 from gps import gps, WATCH_ENABLE, WATCH_NEWSTYLE
+import mdroidconfig
 import time
 import requests
 import sys
@@ -11,36 +12,13 @@ import json
 
 gpsd = gps(mode=WATCH_ENABLE|WATCH_NEWSTYLE) 
 
-def parseConfig():
-	# parse program arguments
-	parser = argparse.ArgumentParser(description='Read from GPSd, forward to REST API.')
-	parser.add_argument('--settings-file', action='store', required=True, help='Config file to load API settings.')
-	args  = parser.parse_args()
-
-	# Overwrite defaults if settings file is provided
-	if args and args.settings_file:
-		if os.path.isfile(args.settings_file): 
-			try:
-				with open(args.settings_file) as json_file:
-					data = json.load(json_file)
-					if "MDROID" in data:
-						# Setup MDroid API
-						if "MDROID_HOST" in data["MDROID"]:
-							return data["MDROID"]["MDROID_HOST"]
-						else:
-							logging.debug("MDROID_HOST not found in config file, not using MDroid API.")
-
-			except IOError as e:
-				logging.error("Failed to open settings file:"+args.settings_file)
-				logging.error(e)
-		else:
-			logging.error("Could not load settings from file"+str(args.settings_file))
-	return False
-   
 try:
 	# Read shared config file first
-	LOGGING_ADDRESS = parseConfig()
-	
+	config = mdroidconfig.readConfig('{"MDROID": {"MDROID_HOST"}}')
+	if config is None:
+		exit("Failed to parse config.")
+	LOGGING_ADDRESS = config["MDROID"]["MDROID_HOST"]
+
 	if LOGGING_ADDRESS:
 		print("Using "+LOGGING_ADDRESS)
 		while True:
