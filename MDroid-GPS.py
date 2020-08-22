e1bb9a83927ee9ed6b6daff837cb31f8b86a9871
qcasey <qcasey1@gmail.com>
2020-08-22 17:22:42 +0000

    Post data individually

--- MDroid-GPS.py
+++ MDroid-GPS.py
@@ -13,35 +13,36 @@ import json
 
 gpsd = gps(mode=WATCH_ENABLE|WATCH_NEWSTYLE) 
 
+def postGPSFix(key, value):
+	try:
+		r = requests.post(LOGGING_ADDRESS+"/session/gps."+key, json = { "value": value }, headers={'Content-type': 'application/json', 'Accept': 'text/plain'})
+	except Exception as e:
+		print("Error in request: "+str(e))
+
 try:
 	# Read shared config file first
-	config = mdroidconfig.readConfig('{"MDROID": {"MDROID_HOST"}}')
-	if config is None:
-		exit("Failed to parse config.")
-	LOGGING_ADDRESS = config["MDROID"]["MDROID_HOST"]
+	#config = mdroidconfig.readConfig('{"MDROID": {"MDROID_HOST"}}')
+	#if config is None:
+	#	exit("Failed to parse config.")
+	LOGGING_ADDRESS = "http://localhost:5353"
 
 	if LOGGING_ADDRESS:
 		print("Using "+LOGGING_ADDRESS)
 		while True:
-			report = gpsd.next() #
-			if report['class'] == 'TPV':
-				postdata = {
-					"latitude": str(getattr(report,'lat','')),
-					"longitude": str(getattr(report,'lon','')),
-					"altitude": str(getattr(report,'alt',None)),
-					"speed": str(getattr(report,'speed',None)),
-					"climb": str(getattr(report,'climb',None)),
-					"epv": str(getattr(report,'epv',None)),
-					"ept": str(getattr(report,'ept',None)),
-					"time": str(getattr(report,'time',''))
-				}
+			try:
+				report = gpsd.next() #
+				if report['class'] == 'TPV':
+					postGPSFix("latitude", str(getattr(report,'lat','')))
+					postGPSFix("longitude", str(getattr(report,'lon','')))
+					postGPSFix("altitude", (getattr(report,'alt',None)))
+					postGPSFix("speed", (getattr(report,'speed',None)))
+					postGPSFix("climb", (getattr(report,'climb',None)))
+					postGPSFix("epv", (getattr(report,'epv',None)))
+					postGPSFix("ept", (getattr(report,'ept',None)))
+					postGPSFix("time", str(getattr(report,'time','')))
 
-				#print(postdata)
+			except Exception as e:
+				print("GPSD error: "+str(e))
 
-				try:
-					r = requests.post(LOGGING_ADDRESS+"/session/gps", json = postdata, headers={'Content-type': 'application/json', 'Accept': 'text/plain'})
-				except Exception as e:
-					print("Error in request: "+str(e))
- 
 except (KeyboardInterrupt, SystemExit): #when you press ctrl+c
 	print("Done.\nExiting.")
